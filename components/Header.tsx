cf9358142c2078b61757ec834261db9b189bcf9f
John Ventura <jpventura925@gmail.com>
2023-09-15 17:51:14 -0400

    added basic slideshow logic

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,43 @@
+'use client';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import { useState, useEffect } from 'react';
 
 export default function Header() {
+  const [start, setStart] = useState(false);
+  const router = useRouter();
+  const path = usePathname();
+  const currentPage = parseInt(path.split('/').slice(-1)[0]);
+
+  useEffect(() => {
+    if (start) {
+      const timer = setTimeout(() => {
+        router.push(`/gallery/${(currentPage + 1) % 15}`);
+      }, 10000);
+      return () => clearTimeout(timer);
+    }
+  }, [path, start]);
+
+  const handleStartSlideshow = () => {
+    if (!start && path === '/') {
+      router.push('/gallery/0');
+    }
+    setStart(!start);
+  };
+
   return (
     <header className="flex w-full items-center justify-between border-b-2 border-gray-200 p-6 xl:max-w-7xl xl:px-0">
-      <Link href="/" className="text-3xl font-bold">
+      <Link
+        onClick={() => setStart(false)}
+        href="/"
+        className="text-3xl font-bold"
+      >
         galleria.
       </Link>
-      <Link href="/">START SLIDESHOW</Link>
+      <button onClick={handleStartSlideshow}>
+        {start ? <h1>STOP SLIDESHOW</h1> : <h1>START SLIDESHOW</h1>}
+      </button>
     </header>
   );
 }
